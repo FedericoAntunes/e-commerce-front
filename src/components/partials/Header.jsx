5d0dbd18d809bedf908ed988ba01baa3e56a49a4
Madozito <juanmamadoz@gmail.com>
2023-03-22 10:38:56 -0300

    Update Header.jsx

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,96 +1,89 @@
-
-import { Navbar, Dropdown, Avatar } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Navbar, Dropdown, Avatar } from "flowbite-react";
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-<Navbar
-  fluid={true}
+      <Navbar fluid={true} style={{backgroundColor: "#1F2937" }} >
+        <Navbar.Brand>
+          <img
+            src="https://i.ibb.co/C26bLZQ/no-Hungerlogo2.png"
+            className="mr-3 h-12 sm:h-16"
+            alt="No Hunger Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl text-yellow-200 font-semibold dark:text-white">
+            No Hunger
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline={true}
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded={true}
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">Bonnie Green</span>
+              <span className="block truncate text-sm font-medium">
+                name@flowbite.com
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse style={{hover: "background-color: #111827"}}
+        className = " "
+        
+        >
+          <Link
+            to={"/"}
+            className="block py-2 pr-4 pl-3 md:p-0   text-gray-300  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent md:dark:hover:text-white text-lg     md:hover:text-yellow-200 "
+            style={{fontWeight: "600"}}
+          >
+            Home
+          </Link>
+          <Link
+            to={"/about-us"}
+            className="block py-2 pr-4 pl-3 md:p-0   text-gray-300  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent md:dark:hover:text-white text-lg   md:hover:text-yellow-200 "
+            style={{fontWeight: "600"}}
+          >
+            About
+          </Link>
+          <Link
+            to={"/login"}
+            className="block py-2 pr-4 pl-3 md:p-0   text-gray-300 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent md:dark:hover:text-white text-lg    md:hover:text-yellow-200 "
+            style={{fontWeight: "600"}}
+          >
+            Login
+          </Link>
 
-  className= "bg-yellow-600"
->
-  <Navbar.Brand>
-    <img
-      src="https://i.ibb.co/qCJx7Zs/nohungerlogo.png"
-      className="mr-3 h-20 sm:h-40"
-      alt="No Hunger Logo"
-      
-    />
-    <span className="self-center whitespace-nowrap text-xl text-yellow-200 font-semibold dark:text-white">
-      No Hunger
-    </span>
-  </Navbar.Brand>
-  <div className="flex md:order-2">
-    <Dropdown
-      arrowIcon={false}
-      inline={true}
-      label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}/>}
-    >
-      <Dropdown.Header>
-        <span className="block text-sm">
-          Bonnie Green
-        </span>
-        <span className="block truncate text-sm font-medium">
-          name@flowbite.com
-        </span>
-      </Dropdown.Header>
-      <Dropdown.Item>
-        Dashboard
-      </Dropdown.Item>
-      <Dropdown.Item>
-        Settings
-      </Dropdown.Item>
-      <Dropdown.Item>
-        Earnings
-      </Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item>
-        Sign out
-      </Dropdown.Item>
-    </Dropdown>
-    <Navbar.Toggle />
-  </div>
-  <Navbar.Collapse>
-    <Navbar.Link
-      href="/"
-      active={true}
-      className = "text-lg md:text-gray-700 md:hover:text-yellow-200"
-    >
-      Home
-    </Navbar.Link>
-    <Navbar.Link
-     href="/about-us"
-     className = "text-lg sm:hover:text-yellow-200 md:hover:text-yellow-200 hover:bg-yellow-600"
-     >
+          <Link
+            to={"/register"}
+            className="block py-2 pr-4 pl-3 md:p-0   text-gray-300 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent md:dark:hover:text-white text-lg   md:hover:text-yellow-200 "
+            style={{fontWeight: "600"}}
+          >
+            Register
+          </Link>
 
-      About
-    </Navbar.Link>
-    <Navbar.Link 
-    href="/login"
-    className = "text-lg  sm:hover:text-yellow-200  md:hover:text-yellow-200 hover:bg-yellow-600 "
-    >
-      
-      Login
-    </Navbar.Link>
-    <Navbar.Link 
-    href="/register"
-    className = "text-lg  sm:hover:text-yellow-200  md:hover:text-yellow-200 hover:bg-yellow-600"
-    >
-      Register
-    </Navbar.Link>
-    <Navbar.Link 
-    
-    className = "text-lg  sm:hover:text-yellow-200  md:hover:text-yellow-200 hover:bg-yellow-600"
-    >
-      Contact
-    </Navbar.Link>
-  </Navbar.Collapse>
-</Navbar>
-
-
-
+          <Link className="block py-2 pr-4 pl-3 md:p-0   text-gray-300  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent md:dark:hover:text-white text-lg   md:hover:text-yellow-200 "
+          style={{fontWeight: "600"}}
+          >
+            
+            Contact
+          </Link>
+        </Navbar.Collapse>
+      </Navbar>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
