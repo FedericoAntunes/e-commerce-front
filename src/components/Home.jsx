0c1cf0496fd058f67b97cb6054a098b0cc71e2ae
Sebasan22 <callofsebas@hotmail.com>
2023-03-23 12:09:34 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,37 @@
 import apiCall from "./api/api";
+import { useEffect, useState } from "react";
+
 function Home() {
-  const companies = async () => {
-    const response = await apiCall("/companies", "get");
-    return response;
+  const [companies, setCompanies] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const response = await apiCall("/categories", "get");
+    setCategories(response);
   };
-  return <h2 className="text-red-500">Hello</h2>;
+
+  const getCompanies = async () => {
+    const response = await apiCall("/companies", "get");
+    setCompanies(response);
+  };
+
+  useEffect(() => {
+    getCompanies();
+    getCategories();
+  }, []);
+
+  return (
+    <>
+      {categories &&
+        categories.map((category) => {
+          return <p>{category.name}</p>;
+        })}
+      {companies &&
+        companies.map((company) => {
+          return <p>{company.name}</p>;
+        })}
+    </>
+  );
 }
 
 export default Home;
