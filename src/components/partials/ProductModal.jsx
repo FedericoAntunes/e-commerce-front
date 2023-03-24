748184fdeca941429b21ac9508f8ed616fc5dfbc
Sebasan22 <callofsebas@hotmail.com>
2023-03-24 09:25:47 -0300

    Update ProductModal.jsx

--- src/components/partials/ProductModal.jsx
+++ src/components/partials/ProductModal.jsx
@@ -18,7 +18,7 @@ export default function ProductModal({
       {showModal ? (
         <>
           <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto w-2/5">
+            <div className="relative w-auto my-6 mx-auto w-[30rem]">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
