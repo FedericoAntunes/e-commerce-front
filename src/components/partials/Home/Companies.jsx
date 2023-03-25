6bb838806de50ef87e7e7860557c6c7553d7c63e
Madozito <juanmamadoz@gmail.com>
2023-03-24 21:27:16 -0300

    Update Companies.jsx

--- src/components/partials/Home/Companies.jsx
+++ src/components/partials/Home/Companies.jsx
@@ -25,18 +25,19 @@ function Companies() {
                 class="w-full max-w-md mx-4 mb-8 overflow-hidden"
               >
                 <Link to={`/${company.slug}`}>
-                  <div class="bg-gray-400 rounded-lg shadow-lg overflow-hidden transition duration-500 transform hover:-translate-y-1 hover:scale-105">
-                    <div class="p-4">
+                  <div class="bg-gray-300 rounded-lg shadow-lg overflow-hidden transition duration-500 transform hover:-translate-y-1 hover:scale-105 shadow">
+                    <div class="p-4 flex justify-center">
                       <img
-                        class="object-contain w-full h-40"
+                        class="h-40 rounded-full"
                         src={`${company.logo}`}
                         alt="category-img"
                       />
-                      <h2 class="text-xl font-bold mt-4 text-white">
-                        {company.name}
-                      </h2>
+
                       <p class="text-gray-400">{company.description}</p>
                     </div>
+                    <h2 class="text-xl font-bold mt-4 text-dark">
+                        {company.name}
+                      </h2>
                   </div>
                 </Link>
               </div>
