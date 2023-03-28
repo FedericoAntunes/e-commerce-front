9f1b03681aa239c29348090c0ab6171564afe98c
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-03-28 17:43:44 -0300

    Added increase quantity value if product is already in cart

--- src/redux/slice/shoppingListSlice.js
+++ src/redux/slice/shoppingListSlice.js
@@ -1,20 +1,28 @@
-
-import { createSlice, current } from '@reduxjs/toolkit';
+import { createSlice, current } from "@reduxjs/toolkit";
 
 export const shoppingListSlice = createSlice({
-  name: 'shoppingList',
+  name: "shoppingList",
   initialState: [],
   reducers: {
     addItem: (state, action) => {
-     state.push(action.payload);
-     console.log(current(state))
-     return state;
+      const product = action.payload;
+      const alreadyInCart = state.some((item) => item.id === product.id);
+      if (alreadyInCart) {
+        const productInCart = state.find((item) => item.id === product.id);
+        productInCart.quantity = productInCart.quantity + product.quantity;
+        return state;
+      }
+      state.push(action.payload);
+      console.log(current(state));
+      return state;
     },
     removeItem: (state, action) => {
       state = state.filter((item) => item.id !== action.payload.id);
     },
     updateItem: (state, action) => {
-      const itemIndex = state.findIndex((item) => item.id === action.payload.id);
+      const itemIndex = state.findIndex(
+        (item) => item.id === action.payload.id
+      );
       state[itemIndex] = action.payload;
     },
   },
