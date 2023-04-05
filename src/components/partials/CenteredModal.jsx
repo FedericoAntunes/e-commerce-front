3004b169693722d4aec249a76960e12870ed81c6
Madozito <juanmamadoz@gmail.com>
2023-04-05 20:08:00 -0300

    Update CenteredModal.jsx

--- src/components/partials/CenteredModal.jsx
+++ src/components/partials/CenteredModal.jsx
@@ -40,51 +40,70 @@ function CenteredModal({
     <>
       {isModalOpen && product.slug === actualProduct ? (
         <>
-          <div className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-75 z-50"></div>
-          <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50">
-            <div className="bg-white rounded-lg w-auto mx-3 md:mx-auto shadow-lg z-50">
-              <div className="border-b border-gray-300 rounded-t-lg p-4">
-                <h3 className="font-semibold text-lg">{product.title}</h3>
-                <button
-                  className="float-right text-xl font-bold leading-none hover:text-black bg-transparent py-1 px-2"
-                  onClick={() => setIsModalOpen()}
-                >
-                  <span className="text-black text-3xl block">×</span>
-                </button>
-              </div>
-              <div className="p-4">
-                <img
-                  className="mx-auto rounded-t-lg"
-                  src={`${product.image}`}
-                  alt="category-img"
-                />
-                <p className="my-4 text-gray-800 text-base leading-relaxed">
-                  {product.description}
-                </p>
-                <h1>In stock: {product.stock}</h1>
-                <div className="text-right">
-                  <h1>US$ {product.price}</h1>
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center">
+            <div className="relative w-full max-w-2xl mx-auto">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-center p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold w-80 text-center">
+                    {product.title}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setIsModalOpen()}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
                 </div>
-                <input
-                  onChange={(e) => handleQuantity(e)}
-                  type="number"
-                  min={1}
-                  max={product.stock}
-                  value={quantity}
-                />
-              </div>
-              <div className="border-t border-gray-300 rounded-b-lg p-4 flex justify-end">
-                <button
-                  onClick={() => {
-                    handleSubmit(product);
-                  }}
-                  className="text-white bg-yellow-600 hover:bg-yellow-700 font-bold py-2 px-4 rounded-full"
-                >
-                  Add to my cart
-                </button>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <img
+                    className="h-40 mx-auto rounded-t"
+                    src={`${product.image}`}
+                    alt="category-img"
+                  />
+                  <p className="my-4 text-slate-500 text-lg mx-auto leading-relaxed w-80">
+                    {product.description}
+                  </p>
+                  <h1>In stock: {product.stock}</h1>
+                  <div className="text-right">
+                    <h1>US$ {product.price}</h1>
+                  </div>
+                  <input
+                    onChange={(e) => handleQuantity(e)}
+                    type="number"
+                    min={1}
+                    max={product.stock}
+                    value={quantity}
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex flex-col items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    onClick={() => {
+                      handleSubmit(product);
+                    }}
+                  >
+                    <AddItemBtn />
+                  </button>
+
+                  <button
+                    className="text-red-500 bg-white hover:bg-red-200 border border-red-500 mt-4 inline-block ms-auto rounded background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setIsModalOpen()}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          <div
+            onClick={() => setIsModalOpen()}
+            className="opacity-25 fixed inset-0 z-40 bg-black"
+          ></div>
         </>
       ) : null}
     </>
