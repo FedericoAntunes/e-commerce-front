37c1e0077309868cb2143328a51bef57fb858e7b
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-04-06 02:33:59 -0300

    Added trending restaurant badge
    The badge shows if the company valoration is bigger than 4.5

--- src/components/partials/Home/Companies.jsx
+++ src/components/partials/Home/Companies.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function Companies({ companies }) {
+  console.log(companies);
   return (
     <div className="grid-cols-1 sm:grid-cols-2 mx-4 grid gap-x-8 gap-y-12 border-t pt-8 mt-4">
       {companies.map((company, index) => (
@@ -10,12 +11,19 @@ function Companies({ companies }) {
           <div className="mx-2 rounded-lg overflow-hidden border shadow-lg">
             <div
               style={{ backgroundImage: `url("${company.background}")` }}
-              className="h-28 bg-cover bg-no-repeat"
-            ></div>
+              className="h-28 relative bg-cover bg-no-repeat"
+            >
+              {company.valoration > 4.5 && (
+                <span className="bg-green-500 text-white p-1 rounded-r-full absolute left-0 top-2">
+                  {" "}
+                  Trending restaurant{" "}
+                </span>
+              )}
+            </div>
             <div className="text-left bg-white relative p-4">
               <h5 className="font-bold mb-1 text-lg">{company.name}</h5>
               <span className="absolute right-2 top-6">
-                {(Math.random() + 4).toFixed(1)}
+                {company.valoration}
                 <FontAwesomeIcon
                   className="text-yellow-400 ml-1"
                   icon={faStar}
