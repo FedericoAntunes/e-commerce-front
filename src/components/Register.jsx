6ed4a00cb1452ad9f425af963b4706c2b2571efe
Sebasan22 <callofsebas@hotmail.com>
2023-04-07 16:30:46 -0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import apiCall from "./api/api";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../redux/slice/userSlice";
+import { ToastContainer, toast } from "react-toastify";
 
 function Register() {
   const [firstname, setFirstName] = useState("");
@@ -27,40 +28,40 @@ function Register() {
     const response = await apiCall("/users", "post", formData, {
       "Content-Type": "multipart/form-data",
     });
-    /*     const response = await axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_API_URL}/usuarios/users`,
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    }); */
     console.log(response.data);
     if (response.data === "Fill all the fields.") {
       setError("Fill all the fields.");
-    } else if (response.data === "User already exist.") {
-      setError("User already exist.");
-    } else {
+      return toast.warn("Fill all the fields.", {
+        position: "bottom-right",
+      });
+    }
+    if (response.data === "Username already exist.") {
+      setError("Username already exist.");
+      return toast.warn("Username already exist.", {
+        position: "bottom-right",
+      });
+    }
+    if (response.data === "User email already exist.") {
+      setError("User email already exist.");
+      return toast.warn("User email already exist.", {
+        position: "bottom-right",
+      });
+    }
+    if (response.data === "User stored") {
       const response = await apiCall("/login", "post", {
         email,
         password,
         reg_mode: "email",
       });
-      /*       const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/usuarios/tokens`,
-        {
-          email,
-          password,
-        }
-      ); */
-      console.log(response.data);
       const user = response.data;
       dispatch(loginUser(user));
-      navigate(`/`);
+      return navigate(`/`);
     }
   };
 
   return (
-    <section className="bg-gradient-to-r from-yellow-300 to-yellow-500">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen ">
+    <section className="bg-gradient-to-r from-yellow-300 to-yellow-500 lg:h-screen">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto ">
         <Link
           to="/about-us"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
@@ -71,7 +72,7 @@ function Register() {
             alt="logo"
           />
         </Link>
-        <div className="sm:w-full bg-gray-100 rounded-lg shadow-xl md:mt-0 py-6 lg:w-1/4">
+        <div className="sm:w-full bg-gray-100 rounded-lg shadow-xl md:mt-0 py-6 lg:w-2/4">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
               Create your account
@@ -81,12 +82,12 @@ function Register() {
               className="space-y-4 md:space-y-6"
               action="#"
             >
-              <div>
+              <div className="lg:flex">
                 <label
                   htmlFor="firstname"
-                  className="block mb-2 font-medium text-black text-base tracking-wide "
+                  className="block mb-2 font-medium text-black text-base tracking-wide lg:my-auto mt-4 mx-4"
                 >
-                  Firstname
+                  Firstname:
                 </label>
                 <input
                   onChange={(e) => setFirstName(e.target.value)}
@@ -98,13 +99,11 @@ function Register() {
                   required=""
                   value={firstname}
                 />
-              </div>
-              <div>
                 <label
                   htmlFor="lastname"
-                  className="block mb-2 font-medium text-black text-base tracking-wide "
+                  className="block mb-2 font-medium text-black text-base tracking-wide lg:my-auto mt-4 mx-4"
                 >
-                  Lastname
+                  Lastname:
                 </label>
                 <input
                   onChange={(e) => setLastName(e.target.value)}
@@ -117,12 +116,12 @@ function Register() {
                   value={lastname}
                 />
               </div>
-              <div>
+              <div className="lg:flex">
                 <label
                   htmlFor="username"
-                  className="block mb-2 font-medium text-black text-base tracking-wide "
+                  className="block mb-2 font-medium text-black text-base tracking-wide lg:my-auto mt-4 mx-4"
                 >
-                  Username
+                  Username:
                 </label>
                 <input
                   onChange={(e) => setUserName(e.target.value)}
@@ -134,13 +133,11 @@ function Register() {
                   required=""
                   value={username}
                 />
-              </div>
-              <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 font-medium text-black text-base tracking-wide "
+                  className="block mb-2 font-medium text-black text-base tracking-wide lg:my-auto mt-4 mx-4"
                 >
-                  Email
+                  Email:
                 </label>
                 <input
                   onChange={(e) => setEmail(e.target.value)}
@@ -153,9 +150,9 @@ function Register() {
                   value={email}
                 />
               </div>
-              <div className="relative flex justify-center">
+              <div className="relative lg:flex justify-center">
                 <input
-                  className="opacity-0 absolute inset-0 z-50 cursor-pointer"
+                  className="mt-4"
                   onChange={(e) => setAvatar(e.target.files[0])}
                   multiple
                   name="avatar"
@@ -163,19 +160,10 @@ function Register() {
                   type="file"
                 />
                 <label
-                  for="avatar"
-                  className="text-white px-4 py-2 bg-yellow-500 rounded-lg cursor-pointer text-base tracking-normal block w-40 text-center"
-                >
-                  Choose File
-                </label>
-              </div>
-
-              <div>
-                <label
                   htmlFor="password"
-                  className="block mb-2 font-medium text-black text-base tracking-wide "
+                  className="block mb-2 font-medium text-black text-base tracking-wide mx-4 lg:my-auto mt-4"
                 >
-                  Password
+                  Password:
                 </label>
                 <input
                   onChange={(e) => setPassword(e.target.value)}
@@ -183,14 +171,14 @@ function Register() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  className="w-full bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block lg:w-1/2 p-2.5 "
                   required=""
                   value={password}
                 />
               </div>
               <button
                 type="submit"
-                className="w-full text-base tracking-wide text-white 	bg-yellow-500 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center "
+                className="lg:w-2/4 w-full text-base tracking-wide text-white my-4 bg-yellow-500 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center "
               >
                 Sign up
               </button>
@@ -198,7 +186,7 @@ function Register() {
                 Do you already have an account?
                 <Link
                   to="/login"
-                  className="font-medium text-base text-blue-500 hover:underline ml-1"
+                  className="font-medium text-base text-blue-500 hover:underline ml-1 "
                 >
                   Sign in
                 </Link>
@@ -207,6 +195,7 @@ function Register() {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </section>
   );
 }
