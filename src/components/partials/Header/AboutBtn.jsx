2756ef516b81f3adcbccb81e96f4d75bf44ba65b
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-04-09 04:04:02 -0300

    New About this project button

--- src/components/partials/Header/AboutBtn.jsx
+++ src/components/partials/Header/AboutBtn.jsx
@@ -1,18 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./AboutBtn.css";
 
 function AboutBtn() {
-  return (
-    <button type="button" className="btn z-40 fixed bottom-2 right-6">
-      <p className="text-sm aboutBtn z-50">About this project</p>
-      <div id="container-stars">
-        <div id="stars"></div>
-      </div>
+  const [timer, setTimer] = useState(false);
 
-      <div id="glow">
-        <div className="circle"></div>
-        <div className="circle"></div>
-      </div>
-    </button>
+  useEffect(() => {
+    setTimeout(() => setTimer(!timer), 5000);
+  }, [timer]);
+
+  return (
+    <div
+      className={`fixed bottom-4 flex ${
+        timer && "wobble-hor-bottom"
+      } z-50 right-6`}
+    >
+      <button className="bg-green-500 p-2 font-semibold hover:bg-green-600 transition ease-in-out-300 rounded-full">
+        <p>About this project</p>
+        <svg
+          stroke-width="4"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          class="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </button>
+    </div>
   );
 }
 
