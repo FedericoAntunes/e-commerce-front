17aac2e4212bee59d990e6c1a5ba0bc93b461f77
dsfpablo <dsfpablo@gmail.com>
2023-04-10 14:57:56 -0300

    va fooooooter

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,15 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faTwitter,
   faInstagram,
+  faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <>
-      <footer
+      {/* <footer
         className="py-8 bg-white text-left mt-12"
         style={{ boxShadow: "rgba(181, 129, 108, 0.5) 0px 16px 24px -18px" }}
       >
@@ -70,6 +72,69 @@ function Footer() {
             </div>
           </div>
         </div>
+      </footer> */}
+      <footer className={styles.footer}>
+        <div className={styles.waves}>
+          <div className={styles.wave} id={styles.wave1}></div>
+          <div className={styles.wave} id={styles.wave2}></div>
+          <div className={styles.wave} id={styles.wave3}></div>
+          <div className={styles.wave} id={styles.wave4}></div>
+        </div>
+        <ul className={styles.social_icon}>
+          <li className={styles.social_icon__item}>
+            <Link className={styles.social_icon__link} to="/">
+              <FontAwesomeIcon icon={faFacebook} />
+            </Link>
+          </li>
+          <li className={styles.social_icon__item}>
+            <Link className={styles.social_icon__link} to="/">
+              <FontAwesomeIcon icon={faTwitter} />
+            </Link>
+          </li>
+          <li className={styles.social_icon__item}>
+            <Link className={styles.social_icon__link} to="/">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Link>
+          </li>
+          <li className={styles.social_icon__item}>
+            <Link className={styles.social_icon__link} to="/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.menu}>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="#">
+              About
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="#">
+              Services
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="/about-us">
+              Team
+            </Link>
+          </li>
+          <li className={styles.menu__item}>
+            <Link className={styles.menu__link} to="#">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <p>&copy;2023 HA Academy Sala 4 | All Rights Reserved</p>
+        <img
+          src="https://www.animatedimages.org/data/media/157/animated-fishing-image-0057.gif"
+          alt=""
+          className="absolute top-0 -m-44 z-[1001]"
+        />
       </footer>
     </>
   );
