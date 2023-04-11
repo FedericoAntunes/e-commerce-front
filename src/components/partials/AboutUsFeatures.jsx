884ac531233682fe8954a997d0f5394076f03c23
Sebasan22 <callofsebas@hotmail.com>
2023-04-11 16:05:43 -0300

    Update AboutUsFeatures.jsx

--- src/components/partials/AboutUsFeatures.jsx
+++ src/components/partials/AboutUsFeatures.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function AboutUsFeatures() {
   return (
     <>
-      <div class="block w-[358px] mx-auto sm:mx-0 my-4 sm:my-0 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <div className="block w-[358px] mx-auto sm:mx-0 my-4 sm:my-0 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Features
         </h5>
         <p className="mb-4 text-lg font-medium text-gray-700 dark:text-gray-400 text-left">
