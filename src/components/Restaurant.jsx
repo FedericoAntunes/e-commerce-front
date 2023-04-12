270aff244e1e8b2deba1e20168a49b0fd53479bf
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-04-12 02:24:04 -0300

    Added No stock image when product stock === 0

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -194,7 +194,14 @@ function Restaurant() {
                           />
                         </div>
                         <Link to={`/${product.company.slug}`}>
-                          <div className="md:hover:scale-105 ease-in-out relative w-full h-[200px] mb-5 overflow-hidden duration-300">
+                          <div className="md:hover:scale-105 relative ease-in-out relative w-full h-[200px] mb-5 overflow-hidden duration-300">
+                            {product.stock === 0 && (
+                              <img
+                                className="absolute inset-0"
+                                src="/img/sold_out.png"
+                                alt="Out of stock"
+                              />
+                            )}
                             <img
                               className="pt-6 pb-4 z-0 mx-auto rounded-t-lg"
                               src={product.image}
@@ -287,6 +294,13 @@ function Restaurant() {
                     </div>
                     <Link to={`/${product.company.slug}`}>
                       <div className="md:hover:scale-105 ease-in-out relative w-full h-[200px] mb-5 overflow-hidden duration-300">
+                        {product.stock === 0 && (
+                          <img
+                            className="absolute h-[270px]"
+                            src="https://www.gunslingerlongboards.co.za/images/overlay_image_nostock.png"
+                            alt="Out of stock"
+                          />
+                        )}
                         <img
                           className="pt-6 pb-4 z-0 mx-auto rounded-t-lg"
                           src={product.image}
