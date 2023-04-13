2a61aa730d0b6f48ecfe7f526068cabcffd57e1c
Madozito <juanmamadoz@gmail.com>
2023-04-13 19:23:24 -0300

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -57,7 +57,7 @@ function AboutUs() {
           <CardAbout
             name={"Martin Bentura"}
             image={
-              "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+              "https://i.ibb.co/qxgXxZx/mbentura-removebg-preview.png"
             }
             description={`Lorem ipsum dolor`}
             github={"https://github.com/MartinBentura"}
